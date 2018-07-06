f82230acc569c705e07bc84b30aa957c6d845159
Eugene Bevziuk <bevziukeuge@icloud.com>
2018-07-06 14:29:49 +0300

    Create Main page, contact page , price page.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,49 @@
+$(document).ready(function(){
 
-console.log("hello world!");
+  /*-----header-----*/
+  //HAMBURGER
+  $('.nav-icon').click(function(){
+      $(this).toggleClass('open');
+      $('.main-menu').toggleClass('open-menu');
+  });
+
+  $('.nav-icon').click(function(){
+      $(this).toggleClass('open-menu');
+      $('body').toggleClass('disable-scroll');
+  });
+
+  /*-----header-----*/
+  $('.one-time').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+
+  /*----animate-----*/
+  $(".main-about_animate-left").animated( "fadeInLeft");
+  $(".main-about_animate-right").animated( "fadeInRight");
+
+  $(".main-servises_antimate").animated( "fadeInLeft");
+  $(".main-servises_antimate-1").animated( "fadeInRight");
+  $(".main-servises_antimate-2").animated( "fadeInRight");
+
+  $(".product_1").animated( "fadeInRight");
+  $(".product_2").animated( "fadeInRight");
+  $(".product_3").animated( "fadeInRight");
+  $(".product_4").animated( "fadeInRight");
+
+  $(".product_4").animated( "fadeInRight");
+  /*----animate-----*/
+
+   $('.price__btn').click(function(){
+       $(this).toggleClass('active');
+   });
+
+   $('.price-child__btn').click(function(){
+       $(this).toggleClass('active');
+   });
+
+
+});
